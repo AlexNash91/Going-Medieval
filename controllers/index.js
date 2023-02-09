836785e9727962ea2b1971aa011377a3751878e3
danocide <dan.c.reilly@gmail.com>
2023-02-09 15:15:40 -0500

    changed html template, added assets to public folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
-const userRoutes = require('./user-routes.js')
+const userRoutes = require('./user-routes.js');
 
 router.use('/', homeRoutes);
+router.use(/login)
 
 module.exports = router;
