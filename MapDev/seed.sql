469a0912e609852bb42731f35728ef4a3afadb41
Eric Graves <graves.eric@gmail.com>
2023-02-09 15:43:35 -0600

    updated mapdev assets and test_db

--- MapDev/seed.sql
+++ MapDev/seed.sql
@@ -1,11 +1,5 @@
-INSERT INTO mapset (res, gfx, own)
+INSERT INTO mapset (loc, spr, def, res, own)
 VALUES
-    ('food', 'field', NULL),
-    ('food', 'field', NULL),
-    ('food', 'field', NULL),
-    ('stone', 'rock', NULL),
-    ('wood', 'forest', NULL),
-    ('iron', 'node', NULL),
-    ('food', 'field', NULL),
-    ('food', 'field', NULL),
-    ('food', 'field', NULL),
+    ('50,45','0001','0001','food++', NULL),
+    ('125,88','0019','0001','rock++', NULL),
+    ('50,131','0009','0001','wood++', NULL);
