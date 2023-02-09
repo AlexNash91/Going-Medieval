0fec8492a169dcd9365cdee5ff6e73b9e4cafd19
boy-son <branden.wheat@yahoo.com>
2023-02-09 10:58:11 -0500

    added the rest of the basic tiles to MapTiles

--- Models/MapTiles.js
+++ Models/MapTiles.js
@@ -4,31 +4,39 @@ const sequelize = require("../config/connection");
 class MapTiles extends Model {}
 
 MapTiles.init(
-{
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,  
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    field: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    forest: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sand: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    water: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  field: {
-type: DataTypes.INTEGER,
-allowNull: false,
-  },
-  forest: {
- type: DataTypes.INTEGER,
- allowNull: false,
-  },
-  rock: {
-type: DataTypes.INTEGER,
-allowNull: false,
-  },  
-},
-{
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "mapset",  
-}    
-)
+    modelName: "mapset",
+  }
+);
 module.exports = MapTiles;
