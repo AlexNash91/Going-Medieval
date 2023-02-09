8f0f8bd44f0b8457a3ad70322af58de29864ac7e
boy-son <branden.wheat@yahoo.com>
2023-02-09 12:48:33 -0500

    added relations for the Player model in the index file

--- Models/index.js
+++ Models/index.js
@@ -9,6 +9,23 @@ const Units = require('./Units');
 const MapTiles = require('./MapTiles');
 const Players = require('./Players');
 
+Players.hasOne(Wood, {
+foreignKey: 'wood_count',    
+});
 
+Players.hasOne(Stone, {
+foreignKey: 'stone_count',    
+});
+
+Players.hasOne(Iron, {
+foreignKey: 'iron_count'    
+});
+Players.hasOne(Food, {
+foreignKey: 'food_count',    
+});
+
+Players.hasOne(Coin, {
+foreignKey: 'coin_count'    
+});
 
 module.exports = { User, Map, Wood, Stone, Iron, Food, Coin, Units, MapTiles, Players};
