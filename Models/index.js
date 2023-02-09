d4740516087f7c91976a4cb5c630f28a1d601186
boy-son <branden.wheat@yahoo.com>
2023-02-09 13:17:25 -0500

    added proper one-to-one associations

--- Models/index.js
+++ Models/index.js
@@ -9,21 +9,27 @@ const Units = require('./Units');
 const MapTiles = require('./MapTiles');
 const Players = require('./Players');
 
+Wood.hasOne(Players);
 Players.belongsTo(Wood, {
 foreignKey: 'wood_count',    
 });
 
+Stone.hasOne(Players);
 Players.belongsTo(Stone, {
 foreignKey: 'stone_count',    
 });
 
+Iron.hasOne(Players);
 Players.belongsTo(Iron, {
 foreignKey: 'iron_count'    
 });
+
+Food.hasOne(Players);
 Players.belongsTo(Food, {
 foreignKey: 'food_count',    
 });
 
+Coin.hasOne(Players);
 Players.belongsTo(Coin, {
 foreignKey: 'coin_count'    
 });
