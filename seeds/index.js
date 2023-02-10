ad0c9dd41a723f1426bc0ea63377af5cfe53983f
Alex Nash <Alexr.nash@gmail.com>
2023-02-10 13:31:34 -0500

    fixed handlebars and login route

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const mapData = require('./mapData');
 const resourcesData = require('./resourcesData');
 const unitsData = require('./unitsData')
 const userData = require('./userData')
+const playerData = require('./PlayerData')
 
 
 const seedAll = async () => {
