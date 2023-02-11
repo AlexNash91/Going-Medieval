18f5c09177c1fd045eb3602b25c6f9bcee54e722
Alex Nash <Alexr.nash@gmail.com>
2023-02-11 00:12:26 -0500

    set up basis for html rendering

--- public/js/Ranks.js
+++ public/js/Ranks.js
@@ -3,13 +3,26 @@ const ranks = function () {
         .then(resp => resp.json())
         .then(data => {
             console.log(data)
-            let displayStrings = "";
+            
             for (var i = 0; i < data.length; i++) {
                 console.log(data[i])
                 console.log('t')
-                console.log("Name: " + data[i].name + " Kingdom: " + data[i].kingdom + " Ranking: " + data[i].ranking)
+                // console.log("Name: " + data[i].name + " Kingdom: " + data[i].kingdom + " Ranking: " + data[i].ranking)
+                let row = document.createElement('tr')
 
-                displayStrings += `<p>${data[i].name + data[i].kingdom + data[i].ranking}</p>`;
+                let cell_1 = document.createElement('th')
+                cell_1.innerHTML = ;
+
+
+                let cell_2 = document.createElement('td')
+                cell_2.innerHTML = ;
+
+
+
+                let cell_3 = document.createElement('td')
+                cell_3.innerHTML = ;
+
+                
             }
 
             const displayContainer = document.getElementById('ranking');
@@ -21,3 +34,90 @@ const ranks = function () {
 }
 
 ranks()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ranks = function () {
+//     fetch('/ranks')
+//         .then(resp => resp.json())
+//         .then(data => {
+//             console.log(data)
+//             let displayStrings = "";
+//             for (var i = 0; i < data.length; i++) {
+//                 console.log(data[i])
+//                 console.log('t')
+//                 console.log("Name: " + data[i].name + " Kingdom: " + data[i].kingdom + " Ranking: " + data[i].ranking)
+
+//                 displayStrings += `<p>${data[i].name + data[i].kingdom + data[i].ranking}</p>`;
+//             }
+
+//             const displayContainer = document.getElementById('ranking');
+//             displayContainer.innerHTML = displayStrings;
+
+//         })
+//         .catch(err => console.log(err))
+
+// }
+
+// ranks()
