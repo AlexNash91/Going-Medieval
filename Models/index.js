a47c0c4af366a1fa726a38fab7433b5b5625d75c
boy-son <branden.wheat@yahoo.com>
2023-02-13 16:11:21 -0500

    updating both User and Players is now working, it was getting caught on the rest of the Players table so I updated it with default values

--- Models/index.js
+++ Models/index.js
@@ -2,8 +2,5 @@ const User = require('./User');
 const Players = require('./Players');
 const Mapset = require('./Mapset')
 
-User.hasMany(Players);
-Players.belongsTo(User);
-
 
 module.exports = { User, Players, Mapset, };
