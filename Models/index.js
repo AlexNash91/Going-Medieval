08210c36a358fce74b17054c36188db1603e7461
Alex Nash <Alexr.nash@gmail.com>
2023-02-12 19:09:32 -0500

    added index requires

--- Models/index.js
+++ Models/index.js
@@ -1,6 +1,11 @@
 const User = require('./User');
 const Players = require('./Players');
 const Mapset = require('./Mapset')
+const Units = require('./troops/units')
+const Archer = require('./troops/archer')
+const Soldier = require('./troops/soldier')
+const Knight = require('./troops/knight')
 
 
-module.exports = { User, Players, Mapset};
+
+module.exports = { User, Players, Mapset, Units, Archer, Soldier, Knight};
