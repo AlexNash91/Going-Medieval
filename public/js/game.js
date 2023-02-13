8c5b8fe54420089945c9bb548937440e65237ddf
Alex Nash <Alexr.nash@gmail.com>
2023-02-13 13:59:31 -0500

    added commented out code for all buttons

--- public/js/game.js
+++ public/js/game.js
@@ -54,30 +54,68 @@ gameScene.create = function () {
             .catch(error => console.error(error));
     });
 
-    //BUTTON BLOCK: section that adds code for all conditional buttons 
-    let atkBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
-    atkBtn.setStrokeStyle(2, 0xffffff);
-    let buttonText = self.add.text(160,25,'Attack', { font: '24px Arial', fill: '#ffffff' });
-    buttonText.setOrigin(0.5);
-    atkBtn.setInteractive();
-    atkBtn.on("pointerdown", function () {    
-    })
+    // BUTTON BLOCK: section that adds code for all conditional buttons 
+    
+    // let atkBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // atkBtn.setStrokeStyle(2, 0xffffff);
+    // let buttonText = self.add.text(160,25,'Attack', { font: '24px Arial', fill: '#ffffff' });
+    // buttonText.setOrigin(0.5);
+    // atkBtn.setInteractive();
+    // atkBtn.on("pointerdown", function () { 
+    //     console.log('Button clicked!');
+    //     // start attack time function
 
-    let impBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
-    atkBtn.setStrokeStyle(2, 0xffffff);
-    let impBtnText = self.add.text(160,25,'Improve Troops', { font: '24px Arial', fill: '#ffffff' });
-    impBtnText.setOrigin(0.5);
-    impBtn.setInteractive();
-    impBtn.on("pointerdown", function () {    
-    })
+    // })
 
-    let genBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
-    genBtn.setStrokeStyle(2, 0xffffff);
-    let genBtnText = self.add.text(160,25,'Train Troops', { font: '24px Arial', fill: '#ffffff' });
-    genBtnText.setOrigin(0.5);
-    genBtn.setInteractive();
-    genBtn.on("pointerdown", function () {    
-    })
+    // let impBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // atkBtn.setStrokeStyle(2, 0xffffff);
+    // let impBtnText = self.add.text(160,25,'Improve Troops', { font: '24px Arial', fill: '#ffffff' });
+    // impBtnText.setOrigin(0.5);
+    // impBtn.setInteractive();
+    // impBtn.on("pointerdown", function () {   
+    //     console.log('Button clicked!'); 
+    //     // update stats of troops 
+    // })
+
+    // let genBtn = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // genBtn.setStrokeStyle(2, 0xffffff);
+    // let genBtnText = self.add.text(160,25,'Train Troops', { font: '24px Arial', fill: '#ffffff' });
+    // genBtnText.setOrigin(0.5);
+    // genBtn.setInteractive();
+    // genBtn.on("pointerdown", function () {    
+    //     console.log('Button clicked!');
+    //     // calls function to bring up unit creation options
+    // })
+
+    // let genSol = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // genBtn.setStrokeStyle(2, 0xffffff);
+    // let genSolText = self.add.text(160,25,'Train Soldier', { font: '24px Arial', fill: '#ffffff' });
+    // genSolText.setOrigin(0.5);
+    // genSol.setInteractive();
+    // genSol.on("pointerdown", function () {    
+    //     console.log('Button clicked!');
+    //     // update number of soldiers in player db
+    // })
+
+    // let genArc = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // genBtn.setStrokeStyle(2, 0xffffff);
+    // let genArcText = self.add.text(160,25,'Train Archer', { font: '24px Arial', fill: '#ffffff' });
+    // genArcText.setOrigin(0.5);
+    // genArc.setInteractive();
+    // genArc.on("pointerdown", function () {    
+    //     console.log('Button clicked!');
+    //     // update number of archers in player db
+    // })
+
+    // let genKni = self.add.rectangle(160,25, 120, 50, 0x000000);
+    // genBtn.setStrokeStyle(2, 0xffffff);
+    // let genKniText = self.add.text(160,25,'Train Knight', { font: '24px Arial', fill: '#ffffff' });
+    // genKniText.setOrigin(0.5);
+    // genKni.setInteractive();
+    // genKni.on("pointerdown", function () {    
+    //     console.log('Button clicked!');
+    //     // update number of knights in player db
+    // })
 
 
 
