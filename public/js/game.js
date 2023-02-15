dce8eac92945b6e5829aaa285080b74524c18b01
Alex Nash <Alexr.nash@gmail.com>
2023-02-14 23:33:42 -0500

    working attack function

--- public/js/game.js
+++ public/js/game.js
@@ -67,82 +67,22 @@ gameScene.create = function () {
     let buttonText = self.add.text(160, 75, 'Attack', { font: '24px Arial', fill: '#ffffff' });
     buttonText.setOrigin(0.5);
     atkBtn.setInteractive();
-    atkBtn.on("pointerdown", function () {
-        console.log('Button clicked!');
-        attackInit()
-    })
-
-    // IMPROVE SOLDIERS BUTTON
-    let impSol = self.add.rectangle(160, 125, 120, 50, 0x000000);
-    impSol.setStrokeStyle(2, 0xffffff);
-    let impSolText = self.add.text(160, 125, 'Improve Soldiers', { font: '24px Arial', fill: '#ffffff' });
-    impSolText.setOrigin(0.5);
-    impSol.setInteractive();
-    impSol.on("pointerdown", function () {
-        console.log('Button clicked!');
-        // PATCH request for mapset
-
-        // fetch('/api/map', {
-        //     method: 'PATCH',
-        //     headers: {
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ })
-        //   })
-        //     .then(res => res.json())
-        //     .then(data => console.log(data))
-        //     .catch(error => console.error(error));
-
-        // increment data for DAM, DEF, and HP
-    })
-
-    // IMPROVE ARCHERS BUTTON
-    let impArc = self.add.rectangle(160, 175, 120, 50, 0x000000);
-    impArc.setStrokeStyle(2, 0xffffff);
-    let impArcText = self.add.text(160, 175, 'Improve Archers', { font: '24px Arial', fill: '#ffffff' });
-    impArcText.setOrigin(0.5);
-    impArc.setInteractive();
-    impArc.on("pointerdown", function () {
-        console.log('Button clicked!');
-        // PATCH request for mapset
-
-        // fetch('/api/map', {
-        //     method: 'PATCH',
-        //     headers: {
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ })
-        //   })
-        //     .then(res => res.json())
-        //     .then(data => console.log(data))
-        //     .catch(error => console.error(error));
-
-        // increment data for DAM, DEF, and HP
-    })
-
-    // IMPROVE KNIGHTS BUTTON
-    let impKni = self.add.rectangle(160, 225, 120, 50, 0x000000);
-    impKni.setStrokeStyle(2, 0xffffff);
-    let impKniText = self.add.text(160, 225, 'Improve Knights', { font: '24px Arial', fill: '#ffffff' });
-    impKniText.setOrigin(0.5);
-    impKni.setInteractive();
-    impKni.on("pointerdown", function () {
-        console.log('Button clicked!');
-        // PATCH request for mapset
-
-        // fetch('/api/map', {
-        //     method: 'PATCH',
-        //     headers: {
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ })
-        //   })
-        //     .then(res => res.json())
-        //     .then(data => console.log(data))
-        //     .catch(error => console.error(error));
-
-        // increment data for DAM, DEF, and HP 
-    })
+    atkBtn.on('pointerdown', function () {
+        if (activeTile[4] == null) {
+            console.log("Attacking Tile")
+            fetch('/claim', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: username, targeting: activeTile[0] })
+                
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(error => console.error(error));
+        } 
+    });
 
     // TRAIN SOLDIERS BUTTON
     let genSol = self.add.rectangle(160, 275, 120, 50, 0x000000);
@@ -185,7 +125,7 @@ gameScene.create = function () {
             .then(data => console.log(data))
             .catch(error => console.error(error));
     });
-  
+
     // TRAIN KNIGHTS BUTTON
     let genKni = self.add.rectangle(160, 375, 120, 50, 0x000000);
     genKni.setStrokeStyle(2, 0xffffff);
@@ -207,6 +147,79 @@ gameScene.create = function () {
             .catch(error => console.error(error));
     });
 
+    // IMPROVE SOLDIERS BUTTON
+    // let impSol = self.add.rectangle(160, 125, 120, 50, 0x000000);
+    // impSol.setStrokeStyle(2, 0xffffff);
+    // let impSolText = self.add.text(160, 125, 'Improve Soldiers', { font: '24px Arial', fill: '#ffffff' });
+    // impSolText.setOrigin(0.5);
+    // impSol.setInteractive();
+    // impSol.on("pointerdown", function () {
+    //     console.log('Button clicked!');
+    //     // PATCH request for mapset
+
+    //     // fetch('/api/map', {
+    //     //     method: 'PATCH',
+    //     //     headers: {
+    //     //       'Content-Type': 'application/json'
+    //     //     },
+    //     //     body: JSON.stringify({ })
+    //     //   })
+    //     //     .then(res => res.json())
+    //     //     .then(data => console.log(data))
+    //     //     .catch(error => console.error(error));
+
+    //     // increment data for DAM, DEF, and HP
+    // })
+
+    // // IMPROVE ARCHERS BUTTON
+    // let impArc = self.add.rectangle(160, 175, 120, 50, 0x000000);
+    // impArc.setStrokeStyle(2, 0xffffff);
+    // let impArcText = self.add.text(160, 175, 'Improve Archers', { font: '24px Arial', fill: '#ffffff' });
+    // impArcText.setOrigin(0.5);
+    // impArc.setInteractive();
+    // impArc.on("pointerdown", function () {
+    //     console.log('Button clicked!');
+    //     // PATCH request for mapset
+
+    //     // fetch('/api/map', {
+    //     //     method: 'PATCH',
+    //     //     headers: {
+    //     //       'Content-Type': 'application/json'
+    //     //     },
+    //     //     body: JSON.stringify({ })
+    //     //   })
+    //     //     .then(res => res.json())
+    //     //     .then(data => console.log(data))
+    //     //     .catch(error => console.error(error));
+
+    //     // increment data for DAM, DEF, and HP
+    // })
+
+    // // IMPROVE KNIGHTS BUTTON
+    // let impKni = self.add.rectangle(160, 225, 120, 50, 0x000000);
+    // impKni.setStrokeStyle(2, 0xffffff);
+    // let impKniText = self.add.text(160, 225, 'Improve Knights', { font: '24px Arial', fill: '#ffffff' });
+    // impKniText.setOrigin(0.5);
+    // impKni.setInteractive();
+    // impKni.on("pointerdown", function () {
+    //     console.log('Button clicked!');
+    // PATCH request for mapset
+
+    // fetch('/api/map', {
+    //     method: 'PATCH',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ })
+    //   })
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //     .catch(error => console.error(error));
+
+    // increment data for DAM, DEF, and HP 
+    // })
+
+
 
 
     //Fetchs data from the mapset table and builds the map tiles with that data.  Also turns on interactivity and adds pointerover functions.  Note the invocation of IIFE in the pointerover and pointerout functions.  this was required to get those functions to alter the alpha.  That is why "index" is being used in the function instead of "i"
@@ -216,7 +229,7 @@ gameScene.create = function () {
             sprites[i].destroy();
         }
         sprites = []; // Clear the array
-    
+
         fetch('/api/map')
             .then(resp => resp.json())
             .then(data => {
@@ -225,25 +238,25 @@ gameScene.create = function () {
                     sprites.push(window['t' + (i)]); // Add the sprite object to the array
                     window['t' + (i)].setDepth(0);
                     window['t' + (i)].setInteractive();
-    
+
                     let text;  //may be redundant DELETE LATER
                     let id = data[i].id
                     let spr = data[i].spr
                     let res = data[i].res
                     let own = data[i].own
-    
+
                     window['t' + (i)].on("pointerover", (function (index) {
                         return function () {
                             window['t' + (index)].setAlpha(.5);
                         }
                     })(i));
-    
+
                     window['t' + (i)].on("pointerout", (function (index) {
                         return function () {
                             window['t' + (index)].setAlpha(1);
                         }
                     })(i));
-    
+
                     //adds on click functionality to the tile - clears the tint on any tile that isnt the one being clicked
                     window['t' + (i)].on("pointerdown", (function (index) {
                         return function () {
@@ -253,10 +266,10 @@ gameScene.create = function () {
                                 }
                             }
                             activeTile = [id, spr, res, own]
-    
+
                             window['t' + (index)].setTint(0xff00ff);
                             console.log(activeTile)
-    
+
                             // Update the text object with the contents of activeTile
                             self.activeTileText.setText(`ID: ${activeTile[0]} \nSpr: ${activeTile[1]} \nRes: ${activeTile[2]} \nOwn: ${activeTile[3]}`);
                         }
@@ -264,11 +277,11 @@ gameScene.create = function () {
                 }
             })
             .catch(err => console.log(err))
-            console.log("MAP BUILT!")
-        }
+        console.log("MAP BUILT!")
+    }
 
     buildmap();
-    this.time.addEvent({ delay: reloadTime, callback: buildmap, callbackScope: this, loop: true});
+    this.time.addEvent({ delay: reloadTime, callback: buildmap, callbackScope: this, loop: true });
 };
 
 gameScene.update = function () {
@@ -277,44 +290,6 @@ gameScene.update = function () {
 //create new game
 let game = new Phaser.Game(config);
 
-// --------------------------------------------------Attacking functions---------------------------------------------------------------
-attack(); {
-    // FETCH request gets ATK from Mapset
-    // Player ATK must be higher than opponents HP + DEF
-    // determines a winner
 
-    // let ATK = your ATK
-    // let HP = opponents HP
-    // let DEF = opponents DEF
 
-    // if (ATK > HP + DEF ) {
-    //     console.log("You win!")
-    // }
-
-    // PATCH request updates tile assignment
-    //     fetch('/api/map', {
-    //         method: 'PATCH',
-    //         headers: {
-    //           'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ id:, own:  })
-    //       })
-    //         .then(res => res.json())
-    //         .then(data => console.log(data))
-    //         .catch(error => console.error(error));
-}
-
-function attackInit() {
-    // calculate amount of turns to get to selected tile
-    // d=√((x2 – x1)² + (y2 – y1)²)
-    // decrement turns left with each tick
-    const turns = "amount of turns left"
-    // conditional that checks if turns left === 0
-    if (turns === 0) {
-        attack()
-    }
-    // when 0, run attack function  
-}
-
-// timer1.timer
 
